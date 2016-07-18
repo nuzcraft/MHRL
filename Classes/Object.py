148091c515ca1622b9237d4100f8e4b9b5584480
Nuzcraft <nuzcraft@gmail.com>
2016-07-18 13:30:44 -0500

    Update character storage and draw function

--- Classes/Object.py
+++ Classes/Object.py
@@ -2,14 +2,16 @@
 import sys
 sys.path.append('..')
 import libtcodpy as lib
+import Public.Functions as pf
 
 class Object:
-    def __init__(self, x, y, char, name, color):
+    def __init__(self, x, y, char, name, color, orientation):
         self.x = x
         self.y = y
-        self.char = char
+        self.char = char[orientation]
         self.name = name
         self.color = color
+        self.orientation = orientation
 
     def move(self, dx, dy):
         # move by the given amount (dx, dy)
@@ -18,12 +20,14 @@ class Object:
 
     def draw(self, console):
         self.console = console
-        lib.console_set_default_foreground(self.console, self.color)
-        # lib.console_put_char(self.console, self.x, self.y, self.char, lib.BKGND_NONE)
-
         CHAR_HEIGHT = len(self.char)
         CHAR_WIDTH = len(self.char[0])
+        # get the center of the character(s) so we can draw in the right place
+        (x_center, y_center) = pf.get_center(CHAR_HEIGHT, CHAR_WIDTH)
         for y in range(CHAR_HEIGHT):
             for x in range(CHAR_WIDTH):
                 if self.char[x][y] != ' ':
-                    lib.console_put_char(self.console, self.x + y, self.y + x, self.char[x][y], lib.BKGND_NONE)       
+                    lib.console_set_default_foreground(self.console, self.color)
+                    lib.console_put_char(self.console, self.x - x_center + y, self.y - y_center + x, self.char[x][y], lib.BKGND_NONE)
+
+                           

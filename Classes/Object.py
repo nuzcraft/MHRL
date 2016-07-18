90b8951676bfcb8c9078426ff62d6230439b05c4
Nuzcraft <nuzcraft@gmail.com>
2016-07-18 16:09:51 -0500

    Basic player movement.
    I changed the way the object 'character' is grabbed and drawn to the screen. I also added a clear function.

--- Classes/Object.py
+++ Classes/Object.py
@@ -5,10 +5,10 @@ import libtcodpy as lib
 import Public.Functions as pf
 
 class Object:
-    def __init__(self, x, y, char, name, color, orientation):
+    def __init__(self, x, y, base_char, name, color, orientation):
         self.x = x
         self.y = y
-        self.char = char[orientation]
+        self.base_char = base_char
         self.name = name
         self.color = color
         self.orientation = orientation
@@ -20,6 +20,8 @@ class Object:
 
     def draw(self, console):
         self.console = console
+        # define the character to be drawn, from within the base character
+        self.define_char(self.base_char, self.orientation)
         CHAR_HEIGHT = len(self.char)
         CHAR_WIDTH = len(self.char[0])
         # get the center of the character(s) so we can draw in the right place
@@ -30,4 +32,22 @@ class Object:
                     lib.console_set_default_foreground(self.console, self.color)
                     lib.console_put_char(self.console, self.x - x_center + y, self.y - y_center + x, self.char[x][y], lib.BKGND_NONE)
 
-                           
+    def clear(self, console):
+        self.console = console
+        # define the character to be drawn, from within the base character
+        self.define_char(self.base_char, self.orientation)
+        CHAR_HEIGHT = len(self.char)
+        CHAR_WIDTH = len(self.char[0])
+        # get the center of the character(s) so we can draw in the right place
+        (x_center, y_center) = pf.get_center(CHAR_HEIGHT, CHAR_WIDTH)
+        for y in range(CHAR_HEIGHT):
+            for x in range(CHAR_WIDTH):
+                if self.char[x][y] != ' ':
+                    lib.console_set_default_foreground(self.console, self.color)
+                    lib.console_put_char(self.console, self.x - x_center + y, self.y - y_center + x, ' ', lib.BKGND_NONE)
+
+
+
+    def define_char(self, base_char, orientation):
+        # defines the character to be displayed within the base character
+        self.char = self.base_char[self.orientation]                    

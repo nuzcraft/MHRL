fd550e19e3a633e6ff929bd17eef6e22e99eb29a
Nuzcraft <nuzcraft@gmail.com>
2016-07-18 16:13:07 -0500

    Basic player movement.
    I added in some keybindings and placed the player characters into a dictionary, to make it easier to find them.

--- Public/Variables.py
+++ Public/Variables.py
@@ -1,11 +1,17 @@
+import sys
+sys.path.append('..')
+import libtcodpy as lib
 
 # Public.Variables
 # This document defines the public variables used in the project.
 
 ###chr definitions
+# 16 solid right arrow
+# 17 solid left arrow
 # 30 solid up arrow
 # 31 solid down arrow
 # 186 double pipes vertical
+# 205 double pipes horizontal
 
 # Size of the Window
 SCREEN_WIDTH = 80
@@ -19,7 +25,11 @@ MAP_HEIGHT = 40
 LIMIT_FPS = 20
 
 # Current Version
-Version = '0.1'
+Version = 'a:0.1'
+
+# Control variables
+key = lib.Key()
+mouse = lib.Mouse()
 
 # Player Variables
 #Characters -- Must be Square, with an odd number on each side
